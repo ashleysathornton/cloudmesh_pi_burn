7fcb2ff26ba9bf88139eb5817dab5940da054799
rickotten <ottenrichie@gmail.com>
2020-03-10 03:06:21 -0400

    squid

--- deploy-proxy.py
+++ deploy-proxy.py
@@ -8,20 +8,51 @@ ProxyPi is a class that will instantiate a Pi as a proxy server using squid3 as 
 https://www.techrepublic.com/article/how-to-install-and-configure-squid-proxy-server-on-linux/
 
 This class will automate the process linked through class initialization and management methods.
+
+Use 'curl -x http://<squid-proxy-server-IP>:3128  -L http://google.com' to verify it is working
+
+Ensure you are in root user (sudo su) mode.
 """
+
+
 class ProxyPi:
     """
     Usage:
+
+    TODO
+
     """
+
+    # Installs squid
+    # allow localnet http_access
     def __init__(self, verbose=False):
         self.verbose = verbose
         if verbose:
             print("sudo apt-get install squid3")
-        self.setup_squid()
-
-    def setup_squid(self):
         self.install_squid()
         self.allow_localnet()
+        self.restart_squid()
+
+    # Writes a config file to /etc/squid/piproxy.conf and includes it in the main default file (if it exists)
+    # Otherwise it creates the necessary files
+    def make_proxy_conf(self):
+        # Create the config file if they exist
+        try:
+            os.system("sudo touch /etc/squid/conf.d/piproxy.conf")
+
+        except:
+            raise Exception(
+                "There is something wrong with your squid installation")
+
+        config = \
+            """
+        shutdown_lifetime = 5 seconds
+        http_access allow localnet
+        """
+
+        with open('/etc/squid/conf.d/piproxy.conf', 'w') as proxy_file:
+            proxy_file.writelines(config)
+
         self.restart_squid()
 
     def install_squid(self):
@@ -29,51 +60,23 @@ class ProxyPi:
         os.system("sudo apt-get install squid3")
         banner("Finishined installation of Squid")
 
+    def start_squid(self):
+        if self.verbose:
+            banner("Starting Squid Sever!")
+        os.system("sudo service squid start")
+
+    def start_squid(self):
+        if self.verbose:
+            banner("Stopping Squid Sever!")
+        os.system("sudo service squid stop")
+
     def restart_squid(self):
         os.system("sudo service squid restart")
 
-    def allow_localnet(self):
-        # 1407#http_access allow localnet
-        line = os.popen("sudo grep -n 'http_access allow localnet' /etc/squid/squid.conf").read().split()
-        line_ref = line[0].split(":")[0] # ['1407', '#http_access']
 
-        if self.verbose:
-            banner(" ".join(line))
-
-        with open('/etc/squid/squid.conf', 'r') as f:
-            data = f.readlines()
-
-        temp = line[0].split(":")
-
-        if temp[1][0] != "#":
-            # If the line is already uncommented, return
-            if self.verbose:
-                banner("http_access already allowed")
-            return
-            
-        temp[1] = temp[1][1:]
-        line[0] = temp[1]
-        
-        joined = " ".join(line)
-        joined+="\n"
-        data[int(line_ref)-1] = joined
-
-        banner(f"Writing line {' '.join(line)}")
-
-        try:
-            with open('/etc/squid/squid.conf', 'w') as f:
-                f.writelines(data)
-        except IOError:
-            sys.stderr.write('Error: Please be in root user \n')
-
-            
-            
-
-        
-        
-
-
+"""
 def main():
     proxy = ProxyPi(verbose=True)
 
 main()
+"""
